38d9af6c64bcf7f5e3801c4f7cb188be835ffbd6
Shanon McQuay <shanonmcquay@gmail.com>
2012-08-10 15:39:13 +1000

    Alert the user when an event is incoming.

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.listen(3000);
 io.sockets.on('connection', function (socket) {
 
   var events = [
-    { id: 1, latitude: 144.963056, longitude: -37.813611 },
-    { id: 2, latitude: 151.20689,  longitude: -33.873651 },
-    { id: 3, latitude: 153.023502, longitude: -27.470933 },
-    { id: 4, latitude: 115.857339, longitude: -31.952854 },
-    { id: 5, latitude: 138.599945, longitude: -34.928726 },
-    { id: 6, latitude: 122.244327, longitude: -17.951221 },
-    { id: 7, latitude: 130.841769, longitude: -12.46282  }
+    { id: 1, latitude: 144.963056, longitude: -37.813611, amount: "$54.36" },
+    { id: 2, latitude: 151.20689,  longitude: -33.873651, amount: "$100.56" },
+    { id: 3, latitude: 153.023502, longitude: -27.470933, amount: "$689.72" },
+    { id: 4, latitude: 115.857339, longitude: -31.952854, amount: "$201.09" },
+    { id: 5, latitude: 138.599945, longitude: -34.928726, amount: "$156.21" },
+    { id: 6, latitude: 122.244327, longitude: -17.951221, amount: "$104.11" },
+    { id: 7, latitude: 130.841769, longitude: -12.46282,  amount: "$93.99"  }
   ];
 
   socket.emit('event', events.shift());
@@ -25,7 +25,7 @@ io.sockets.on('connection', function (socket) {
     if(events.length > 0) {
       socket.emit('event', events.shift());
     }
-  }, 5000);
+  }, 10000);
 
 });
 
